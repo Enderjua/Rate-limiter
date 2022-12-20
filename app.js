9d7059769b76fb7219f9b0298e1b5b5fd8fc371c
Ender Jua <120639059+Enderjua@users.noreply.github.com>
2022-12-20 22:05:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-const express = require('express');
-const app = express();
+const axios = require('axios');
 
-const rateLimit = require("express-rate-limit");
- 
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minute
-  max: 100 // max 100 req
-});
- 
-app.use(limiter);
+// Bir dakikadaki istek sayısını ve bir çağrının yapılacağı URL'i alan rate limiter fonksiyonu
+function rateLimiter(requestLimit, url) {
+  // Bir dakika süresince yapılan istekleri sayısını tutacak olan sayaç değişkeni
+  let requestsCounter = 0;
 
-app.get('/', (req, res) => {
-  res.send('DDoS Protection Online!.');
+  // Bir dakika süresince yapılacak istekleri sınırlamak için setInterval ile bir dakika aralıklarla çağrı yapılacak
+  setInterval(() => {
+    requestsCounter = 0;
+  }, 60000);
+
+  return function(config) {
+    // Eğer istek sayısı limitini aşıyorsa, bir dakika bekleme süresi uygulanacak
+    if (requestsCounter >= requestLimit) {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 60000);
+      });
+    }
+
+    // Eğer istek URL'si verilen URL ile eşleşiyorsa, sayaç değişkenini arttır ve isteği yap
+    if (config.url === url) {
+      requestsCounter++;
+      return axios(config);
+    }
+
+    // Diğer durumlarda, normal şekilde isteği yap
+    return axios(config);
+  };
+}
+
+// axios örneği oluşturulur ve rateLimiter fonksiyonu transformRequest içine eklenir
+const axiosInstance = axios.create({
+  // ... diğer konfigürasyon seçenekleri ...
+  transformRequest: [
+    // ... diğer transform fonksiyonları ...
+    rateLimiter(5, 'http://api.example.com') // Bir dakikada 5 istek yapılabilecek URL
+  ]
 });
 
-app.listen(3000, () => {
-  console.log('listening 3000 port');
-});
+// axiosInstance ile istek yapılır
+axiosInstance.get('http://api.example.com/items')
+  .then((response) => {
+    // İstek başarılı olduğunda yapılacak işlemler
+  })
+  .catch((error) => {
+    // İstek başarısız olduğunda yapılacak işlemler
+  });
